b305eeb48c612667f8229b0d5721485107c79d00
Edgardo <edgardo951.ef@gmail.com>
2019-05-30 18:18:32 -0300

    Agregue el modulo incognita y el modulo de tabla_de_letras_utilizadas

---
+++ incognita.py
@@ -0,0 +1,35 @@
+#importamos la libreria random para poder elegir utilizar una funcion que eliga una palabra aleatoriamente
+import random
+#funcion incognita
+def incognita():
+	#lista de palabras
+	lista_de_palabras=['Alberto','Albina','Alejandro','Alejo','Alfonso','Alfredo','Alicia','Alipio','Almudena','Alonso']
+	#funcion que elige una palabra de la lista aleatoriamente
+	palabra = random.choice(lista_de_palabras)
+	#se calcula la longitud de la palabra
+	longitud = len(palabra)
+	#agregamos guiones bajos a una lista vacia(la cantidad de guiones que se agregan corresponde a la longitud de la palabra)
+	for x in palabra:
+		lista.append("_")
+	return palabra
+#muestra la lista que generamos con la cantidad de guiones que corresponde a la longitud de la palabra 
+def mostrar_Lista():
+	for i in lista:
+		print(i,end=' ')
+#pide al usuario ingresar una letra
+def ingresa_Letra():
+	letra = input("\n\nIngresar una letra: ")
+	#devolvemosla letra que ingreso el usuario
+	return letra 
+#busca y reemplaza los giones bajos por la letra en el lugar que corresponde
+def buscar_y_Remplazar(letra,palabra):
+	#contador para guardar la posicion
+	posicion = 0
+	#recorremos la palabra
+	for l in palabra:
+		#si l es igual a letra 
+		if l == letra:
+			#se reemplaza en la lista el guion por la letra en la posicion que corresponde
+			lista[posicion]=letra
+		#incrementamos el contador
+		posicion+=1
---
+++ tabla_de_letras_utilizadas.py
@@ -0,0 +1,8 @@
+
+def letras_utilizadas(letra):
+	lista.append(letra)
+	
+def mostar_Letras_Usadas():
+	for i in lista:
+		print(i)
+
