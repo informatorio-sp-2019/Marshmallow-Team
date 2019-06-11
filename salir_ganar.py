dbb0db859079a52116859381abec6f948f68dbf0
alan87 <alanpanzich@hotmail.com>
2019-06-10 22:00:27 -0300

    se hizo tarea vidas, puntuación donde se suma 5 puntos, ventana puntuación, y se arreglaron varios bugs

---
+++ salir_ganar.py
@@ -0,0 +1,7 @@
+from H1_menu_principal import menu, bienvenida
+def salir_ganar():
+    out= input("Presione 1 para salir:  ")
+    if out == "1":
+		H1_menu_principal.bienvenita()
+		H1_menu_principal.menu()
+salir_ganar()
